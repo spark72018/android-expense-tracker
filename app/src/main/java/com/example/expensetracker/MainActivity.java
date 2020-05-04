f79e5c5b6799b06478bcff55201cca19a155796a
Steve <spark89@gmail.com>
2020-05-03 23:05:39 -0400

    some cleanup

--- app/src/main/java/com/example/expensetracker/MainActivity.java
+++ app/src/main/java/com/example/expensetracker/MainActivity.java
@@ -1,18 +1,11 @@
 package com.example.expensetracker;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
-import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,16 +19,22 @@ public class MainActivity extends AppCompatActivity {
 
     private void initLoginButtonListener() {
         final Button loginButton = findViewById(R.id.login_button);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+
+        if (loginButton == null) return;
+
+        loginButton.setOnClickListener(getLoginButtonClickListener());
+    }
+
+    private View.OnClickListener getLoginButtonClickListener() {
+        return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onLoginClick(v);
             }
-        });
+        };
     }
 
     private void onLoginClick(View v) {
-        System.out.println("onLoginClick clicked");
         Intent nextActivity = new Intent(getApplicationContext(), ExpenseFormActivity.class);
         startActivity(nextActivity);
     }
